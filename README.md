3481155867b22dc79ec209abaf29780b2322f778
George <georgect35@gmail.com>
2026-07-30 13:14:37 -0400

    Added emotion_detector and init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Repository for final project
+https://github.com/george-ct52/oaqjp-final-project-emb-ai.git
+Final Project: Emotion Detection
---
+++ emotion_detection.py
@@ -0,0 +1,33 @@
+import requests,json
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response=requests.post(URL,json=input_json,headers=header)
+    formatted_response = json.loads(response.text)
+
+
+
+        
+    if response.status_code==200:
+        emotions = formatted_response["emotionPredictions"][0]["emotion"] 
+        dominant_emotion = max(emotions, key=emotions.get)
+   
+
+        return {
+            "anger": emotions["anger"],
+            "disgust": emotions["disgust"],
+            "fear": emotions["fear"],
+            "joy": emotions["joy"],
+            "sadness": emotions["sadness"],
+            "dominant_emotion": dominant_emotion
+        }    
+    elif response.status_code == 400:
+        formatted_response ={
+            "anger":None,
+            "disgust":None,
+            "fear":None,
+            "joy":None,
+            "sadness": None,
+            "dominant_emotion": None }
+        return formatted_response
